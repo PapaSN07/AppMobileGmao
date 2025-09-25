7b5392611c986e8bf36d8a81c649ea4d46ad49b9
Mouha7 <ra.sow25@gmail.com>
2025-09-25 17:52:10 +0000

    feat : traduction des messages d'erreur et suppression du configurateur flottant dans les pages d'accès et d'erreur

--- src/app/pages/auth/error/error.ts
+++ src/app/pages/auth/error/error.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
-import { AppFloatingConfigurator } from '../../../layout/component/app.floatingconfigurator';
 
 @Component({
   selector: 'app-error',
-  imports: [ButtonModule, RippleModule, RouterModule, AppFloatingConfigurator, ButtonModule],
+  imports: [ButtonModule, RippleModule, RouterModule, ButtonModule],
   standalone: true,
   templateUrl: './error.html'
 })
