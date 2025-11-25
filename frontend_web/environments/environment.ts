b9d38b15246eb4ba14962029db99da01fc9dae66
PapaSN07 <mamadsene14@gmail.com>
2025-11-25 12:31:17 +0000

    Update environment configuration to use production API URLs

--- frontend_web/environments/environment.ts
+++ frontend_web/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     PRODUCTION: false,
-    API_URL: 'http://localhost:8000/api/v1/web',
-    API_URL_BASE: 'http://localhost:8000',
-    API_URL_AUTH: 'http://localhost:8000/api/v1/auth',
-    WEBSOCKET_URL: 'ws://localhost:8000/ws/notifications'
+    API_URL: 'http://srv-applisnlcprod:8000/api/v1/web',
+    API_URL_BASE: 'http://srv-applisnlcprod:8000',
+    API_URL_AUTH: 'http://srv-applisnlcprod:8000/api/v1/auth',
+    WEBSOCKET_URL: 'ws://srv-applisnlcprod:8000/ws/notifications'
 }
