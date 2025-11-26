dcce872269dcd057b902335b9c933583f1f3c040
PapaSN07 <mamadsene14@gmail.com>
2025-11-26 09:06:20 +0000

    deploiementUpdated

--- frontend_web/environments/environment.ts
+++ frontend_web/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    PRODUCTION: false,
-    API_URL: 'http://srv-applisnlcprod:8000/api/v1/web',
-    API_URL_BASE: 'http://srv-applisnlcprod:8000',
-    API_URL_AUTH: 'http://srv-applisnlcprod:8000/api/v1/auth',
-    WEBSOCKET_URL: 'ws://srv-applisnlcprod:8000/ws/notifications'
+    PRODUCTION: true,
+    API_URL: 'http://10.101.2.46:9099/api/v1/web',
+    API_URL_BASE: 'http://10.101.2.46:9099',
+    API_URL_AUTH: 'http://10.101.2.46:9099/api/v1/auth',
+    WEBSOCKET_URL: 'ws://10.101.2.46:9099/ws/notifications'
 }
